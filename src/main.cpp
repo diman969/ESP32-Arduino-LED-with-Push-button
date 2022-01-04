63d4cfbcd09719953649b4c012b4b46aea098f77
diman969 <diman969@gmail.com>
2022-01-04 16:54:53 +0200

    fixed errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <Arduino.h>
-#define ledPin 2
-#define buttonPin 4
+#define LED_PIN 2
+#define BUTTON_PIN 4
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLDOWN);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
   Serial.begin(9600);
-  digitalWrite(ledPin, LOW);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bool isPressed{digitalRead(buttonPin)};
+  bool isPressed{digitalRead(BUTTON_PIN)};
   if(isPressed){
-      digitalWrite(ledPin, HIGH);
+      digitalWrite(LED_PIN, HIGH);
       Serial.println("ON");
   }
   else{
-    digitalWrite(ledPin, LOW);
+    digitalWrite(LED_PIN, LOW);
     Serial.println("OFF");
   }
 }
